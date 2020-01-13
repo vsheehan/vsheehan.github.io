f46bb50841a5068d4f094bca512fd7edf840dac2
Veronica Sheehan <vsheehan@techdisorder.com>
2020-01-13 01:53:02 -0600

    Admin Post JS Added

--- wordpress-common/js/admin-post.js
+++ wordpress-common/js/admin-post.js
@@ -4,13 +4,12 @@
         this.$sortables = $('#after-title-sortables');
         this.$placeholder =$('<div>', { class: 'postbox' }).css({ height: '100px', background: 'transparent', border: '1px dashed #b4b9be', });
         this.$wpSortable = $('.meta-box-sortables');
-        if ( ! this.$sortables.find( '.postbox' ).length ) {
-            this.$wpSortable.on('sortstart', function(event, ui) {
-                that.$sortables.append( that.$placeholder );
-            })
-        }
 
-        this.$wpSortable.on('sortstop', function( event, ui ) {
+        this.$wpSortable.on('sortstart', function(event, ui) {
+            if ( ! this.$sortables.find( '.postbox' ).length ) {
+                that.$sortables.append(that.$placeholder);
+            }
+        }).on('sortstop', function( event, ui ) {
             that.$placeholder.remove();
         });
 
