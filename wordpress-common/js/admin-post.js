998f208c06afb14470b735f9b3d1087f1d83b020
Veronica Sheehan <vsheehan@techdisorder.com>
2020-01-13 01:40:09 -0600

    Admin Post JS Added

--- wordpress-common/js/admin-post.js
+++ wordpress-common/js/admin-post.js
@@ -1,5 +1,19 @@
 (function($) {
-    $('.meta-box-sortables').on('sortstart', function(event, ui) {
-        console.log( 'sorting' );
-    });
+    function afterTitleMetaPlaceholder() {
+        let that = this;
+        this.$sortables = $('#after-title-sortables');
+        this.$placeholder =$('<div>', { class: 'sortable-placeholder' }).css({ height: '100px' });
+
+        $('.meta-box-sortables').on('sortstart', function(event, ui) {
+            console.log( ui );
+
+            that.$sortables.append( that.$placeholder );
+        }).on('sortstop', function( event, ui ) {
+            that.$placeholder.remove();
+        });
+
+    }
+    afterTitleMetaPlaceholder();
+
+
 })(jQuery);
