98bb18d077a23d697595abef2320c3896734d704
Veronica Sheehan <vsheehan@techdisorder.com>
2020-01-13 01:58:36 -0600

    Admin Post JS Added

--- wordpress-common/js/admin-post.js
+++ wordpress-common/js/admin-post.js
@@ -7,10 +7,11 @@
 
         this.$wpSortable.on('sortstart', function(event, ui) {
             if ( ! that.$sortables.find( '.postbox' ).length ) {
-                that.$sortables.append(that.$placeholder);
+                //that.$sortables.append(that.$placeholder);
+                that.$sortables.css({ height: '100px', background: 'transparent', border: '1px dashed #b4b9be', });
             }
         }).on('sortstop', function( event, ui ) {
-            that.$placeholder.remove();
+            that.$sortables.css('');
         });
 
     }
